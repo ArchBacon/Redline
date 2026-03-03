44bbd9f9f9b73c34b6a6ca382e88a565c96f7da5
Christian Delicaat <cdelicaat.me@gmail.com>
2026-03-03 13:27:28 +0100

    Regenerated premake file by giving AI an example of an existing premake file

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 workspace "redline"
+    architecture "x86_64"
     configurations { "Debug", "Release" }
-    platforms { "x64" }
-    startproject "hello"
+    startproject "redline"
     language "C++"
     cppdialect "C++17"
     cdialect "C17"
@@ -27,84 +27,135 @@ workspace "redline"
 
     filter {}
 
-local B = "bee/"
-
-local function bee_common()
-    includedirs {
-        B .. "include", B .. "external", B .. "external/fmt/include",
-        B .. "external/clipper/include", B .. "external/Jolt", B .. "external/glad/include",
-    }
-    defines {
-        "BEE_PROFILE", "BEE_JOLT_PHYSICS", "BEE_PLATFORM_PC",
-        "BEE_GRAPHICS_OPENGL", "GLM_FORCE_SILENT_WARNINGS", "_UNICODE", "UNICODE",
-    }
-    filter "configurations:Debug"  defines { "BEE_DEBUG", "BEE_INSPECTOR" }
-    filter "configurations:Release" defines { "BEE_INSPECTOR" }
-    filter {}
-end
-
-local function game_project(name)
-    kind "ConsoleApp"
-    location(name)
-    targetdir(name .. "/executable/x64/%{cfg.buildcfg}")
-    objdir(name .. "/intermediate/x64/%{cfg.buildcfg}")
-    bee_common()
-    dependson { "bee", "Jolt" }
-    libdirs { B .. "lib/x64/%{cfg.buildcfg}", B .. "external/Jolt/lib/x64/%{cfg.buildcfg}", B .. "external", B .. "external/GLFW" }
-    links { "bee", "Jolt", "opengl32" }
-
-    filter "configurations:Debug"
-        links { "glfw3", "fmod/lib/fmodstudioL_vc", "fmod/lib/fmodL_vc", "Superluminal/PerformanceAPI_MDd" }
-        ignoredefaultlibraries { "MSVCRT" }
-    filter "configurations:Release"
-        links { "glfw3", "fmod/lib/fmodstudio_vc", "fmod/lib/fmod_vc", "Superluminal/PerformanceAPI_MD" }
-    filter {}
-
-    local ext = "%{wks.location}bee/external"
-    postbuildcommands { '{COPYFILE} "' .. ext .. '/Superluminal/PerformanceAPI.dll" "%{cfg.targetdir}"' }
-    filter "configurations:Debug"
-        postbuildcommands {
-            '{COPYFILE} "' .. ext .. '/fmod/lib/fmodL.dll" "%{cfg.targetdir}"',
-            '{COPYFILE} "' .. ext .. '/fmod/lib/fmodstudioL.dll" "%{cfg.targetdir}"',
-        }
-    filter "configurations:Release"
-        postbuildcommands {
-            '{COPYFILE} "' .. ext .. '/fmod/lib/fmod.dll" "%{cfg.targetdir}"',
-            '{COPYFILE} "' .. ext .. '/fmod/lib/fmodstudio.dll" "%{cfg.targetdir}"',
-        }
-    filter {}
-end
-
 project "Jolt"
     kind "StaticLib"
     location "bee"
-    targetdir(B .. "external/Jolt/lib/x64/%{cfg.buildcfg}")
-    objdir(B .. "external/Jolt/intermediate/x64/%{cfg.buildcfg}")
-    includedirs { B .. "external" }
+    targetdir "bee/external/Jolt/lib/x64/%{cfg.buildcfg}"
+    objdir "bee/external/Jolt/intermediate/x64/%{cfg.buildcfg}"
+    includedirs { "bee/external" }
     pchheader "Jolt/Jolt.h"
-    pchsource(B .. "external/Jolt/pch.cpp")
-    files { B .. "external/Jolt/**.h", B .. "external/Jolt/**.cpp", B .. "external/Jolt/**.inl" }
+    pchsource "bee/external/Jolt/pch.cpp"
+    files
+    {
+        "bee/external/Jolt/**.h",
+        "bee/external/Jolt/**.cpp",
+        "bee/external/Jolt/**.inl",
+    }
 
 project "bee"
     kind "StaticLib"
     location "bee"
-    targetdir(B .. "lib/x64/%{cfg.buildcfg}")
-    objdir(B .. "intermediate/x64/%{cfg.buildcfg}")
-    bee_common()
-    files {
-        B .. "source/ai/**.cpp", B .. "source/core/**.cpp", B .. "source/graph/**.cpp",
-        B .. "source/math/**.cpp", B .. "source/physics/**.cpp", B .. "source/rendering/**.cpp",
-        B .. "source/tools/**.cpp", B .. "source/platform/pc/**.cpp", B .. "source/platform/opengl/**.cpp",
-        B .. "include/**.hpp", B .. "include/**.h",
-        B .. "external/clipper/src/**.cpp",
-        B .. "external/imgui/*.cpp",
-        B .. "external/predicates/predicates.cpp",
-        B .. "external/tinygltf/tiny_gltf.cc",
-        B .. "external/glad/src/glad.c",
-        B .. "external/entt/natvis/entt/**.natvis",
-        B .. "external/glm/glm.natvis",
+    targetdir "bee/lib/x64/%{cfg.buildcfg}"
+    objdir "bee/intermediate/x64/%{cfg.buildcfg}"
+
+    includedirs
+    {
+        "bee/include",
+        "bee/external",
+        "bee/external/fmt/include",
+        "bee/external/csv_parser/include",
+        "bee/external/clipper/include",
+        "bee/external/Jolt",
+        "bee/external/glad/include",
     }
 
+    defines
+    {
+        "BEE_PROFILE", "BEE_JOLT_PHYSICS", "BEE_PLATFORM_PC",
+        "BEE_GRAPHICS_OPENGL", "GLM_FORCE_SILENT_WARNINGS", "_UNICODE", "UNICODE",
+    }
+
+    files
+    {
+        "bee/source/ai/**.cpp",
+        "bee/source/core/**.cpp",
+        "bee/source/graph/**.cpp",
+        "bee/source/math/**.cpp",
+        "bee/source/physics/**.cpp",
+        "bee/source/rendering/**.cpp",
+        "bee/source/tools/**.cpp",
+        "bee/source/platform/pc/**.cpp",
+        "bee/source/platform/opengl/**.cpp",
+        "bee/include/**.hpp",
+        "bee/include/**.h",
+        "bee/external/clipper/src/**.cpp",
+        "bee/external/imgui/*.cpp",
+        "bee/external/predicates/predicates.cpp",
+        "bee/external/tinygltf/tiny_gltf.cc",
+        "bee/external/glad/src/glad.c",
+        "bee/external/entt/natvis/entt/**.natvis",
+        "bee/external/glm/glm.natvis",
+    }
+
+    filter "configurations:Debug"
+        defines { "BEE_DEBUG", "BEE_INSPECTOR" }
+    filter "configurations:Release"
+        defines { "BEE_INSPECTOR" }
+    filter {}
+
 project "redline"
-    game_project("redline")
-    files { "redline/**.cpp", "redline/**.hpp" }
+    kind "ConsoleApp"
+    location "redline"
+    targetdir "redline/executable/x64/%{cfg.buildcfg}"
+    objdir "redline/intermediate/x64/%{cfg.buildcfg}"
+    dependson { "bee", "Jolt" }
+
+    includedirs
+    {
+        "bee/include",
+        "bee/external",
+        "bee/external/fmt/include",
+        "bee/external/csv_parser/include",
+        "bee/external/clipper/include",
+        "bee/external/Jolt",
+        "bee/external/glad/include",
+    }
+
+    defines
+    {
+        "BEE_PROFILE", "BEE_JOLT_PHYSICS", "BEE_PLATFORM_PC",
+        "BEE_GRAPHICS_OPENGL", "GLM_FORCE_SILENT_WARNINGS", "_UNICODE", "UNICODE",
+    }
+
+    files
+    {
+        "redline/**.cpp",
+        "redline/**.hpp",
+        "redline/**.h",
+    }
+
+    libdirs
+    {
+        "bee/lib/x64/%{cfg.buildcfg}",
+        "bee/external/Jolt/lib/x64/%{cfg.buildcfg}",
+        "bee/external",
+        "bee/external/GLFW",
+    }
+
+    links { "bee", "Jolt", "opengl32" }
+
+    postbuildcommands
+    {
+        '{COPYFILE} "%{wks.location}bee/external/Superluminal/PerformanceAPI.dll" "%{cfg.targetdir}"',
+    }
+
+    filter "configurations:Debug"
+        defines { "BEE_DEBUG", "BEE_INSPECTOR" }
+        links { "glfw3", "fmod/lib/fmodstudioL_vc", "fmod/lib/fmodL_vc", "Superluminal/PerformanceAPI_MDd" }
+        ignoredefaultlibraries { "MSVCRT" }
+        postbuildcommands
+        {
+            '{COPYFILE} "%{wks.location}bee/external/fmod/lib/fmodL.dll" "%{cfg.targetdir}"',
+            '{COPYFILE} "%{wks.location}bee/external/fmod/lib/fmodstudioL.dll" "%{cfg.targetdir}"',
+        }
+
+    filter "configurations:Release"
+        defines { "BEE_INSPECTOR" }
+        links { "glfw3", "fmod/lib/fmodstudio_vc", "fmod/lib/fmod_vc", "Superluminal/PerformanceAPI_MD" }
+        postbuildcommands
+        {
+            '{COPYFILE} "%{wks.location}bee/external/fmod/lib/fmod.dll" "%{cfg.targetdir}"',
+            '{COPYFILE} "%{wks.location}bee/external/fmod/lib/fmodstudio.dll" "%{cfg.targetdir}"',
+        }
+
+    filter {}
